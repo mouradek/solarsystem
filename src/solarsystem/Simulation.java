002e1abd6d6200c61f560dc4d68977cbe6dfd79f
mourad <mourad@Moozy.mo-home.net>
2017-07-05 20:21:11 +0200

    Klassengerüste Simulation und SolarSystem zugefügt.

---
+++ src/solarsystem/Simulation.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package solarsystem;
+
+/**
+ * @author mourad
+ *
+ */
+public class Simulation {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SolarSystem sol = new SolarSystem();
+		sol.run();
+	}
+
+}
---
+++ src/solarsystem/SolarSystem.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package solarsystem;
+
+/**
+ * @author mourad
+ *
+ */
+public class SolarSystem {
+	
+	void run() {
+		while(true) {
+			
+		}
+	}
+
+}
